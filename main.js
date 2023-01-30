7b6bac851629c701a11aae56f0b0e4e8f0571d55
Damien Carcel <damien.carcel@akeneo.com>
2023-01-30 14:46:35 +0100

    Finish first part of the slides

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
+import 'reveal.js/dist/theme/league.css';
 /* Code syntax highlighting */
-import 'reveal.js/plugin/highlight/zenburn.css';
+import 'reveal.js/plugin/highlight/monokai.css';
 
 import Reveal from 'reveal.js';
+import RevealHighlight from 'reveal.js';
+import RevealNotes from 'reveal.js';
 
-const deck = new Reveal();
+const deck = new Reveal({
+  plugins: [RevealHighlight, RevealNotes],
+});
 deck.initialize();
